f080eeb59b529c084b97c8b3c9c2d17b29ed31a7
josephguluzada <yquluzade98@gmail.com>
2022-10-31 17:41:32 +0400

    added loops task solutions

---
+++ 31-10-2022/AP105-Loops/AP105-Loops/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace AP105_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Homework 1
+            //var i = 1;
+            //var sum = 0;
+            //while (i<10)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //    i++;
+            //}
+            //Console.WriteLine("cem: " + sum);
+            #endregion
+
+            #region While
+            //var i = 1;
+            //var sum = 0;
+
+            //while (i < 100)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //    i++;
+            //}
+            //Console.WriteLine("cem: " + sum);
+            //Console.WriteLine("i'nin deyeri: " + i);
+            #endregion
+
+            #region For
+            //int sum2 = 0;
+            //for (int j = 1; j < 100; j++)
+            //{
+            //    if (j % 5 == 0)
+            //    {
+            //        sum2 += j;
+            //    }
+            //}
+            //Console.WriteLine(sum2);
+            #endregion
+
+            #region Do-while
+            //var i = 1;
+            //var sum = 0;
+            //do
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //    i++;
+            //} while (i<100);
+            #endregion
+
+            #region Continue
+            // 1 den 10 qeder reqemler arasinda 3 var mi?
+
+            //int i = 1;
+
+            //while (i < 10)
+            //{
+            //    if (i == 3)
+            //    {
+            //        i++;
+            //        Console.WriteLine("*** ***");
+            //        continue;
+            //    }
+            //    //Console.WriteLine(i);
+            //    Console.WriteLine("Yusif" + i);
+            //    i++;
+            //}
+            #endregion
+
+            #region Break
+            // 1 den 10 qeder reqemler arasinda 3 var mi?
+
+            //int i = 1;
+
+            //while (i < 10)
+            //{
+            //    if (i == 3)
+            //    {
+            //        Console.WriteLine("3 var");
+            //        break;
+            //    }
+            //    i++;
+            //}
+            #endregion
+        }
+    }
+}
---
+++ 31-10-2022/Loops in js/index.js
@@ -0,0 +1,19 @@
+// var i = 1;
+// var sum = 0;
+// while (i < 10) {
+//     if (i % 2 == 1) {
+//         sum += i;
+//     }
+//     i++;
+// }
+// console.log("cem: " + sum);
+
+
+
+let sum2 = 0;
+for (let j = 1; j < 100; j++) {
+    if (j % 5 == 0) {
+        sum2 += j;
+    }
+}
+console.log(sum2);
